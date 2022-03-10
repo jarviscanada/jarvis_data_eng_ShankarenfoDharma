7892bb2615c310b823ba3b85d9a0b05f5867003c
Shanka Dharma <shankarenfo@gmail.com>
2022-03-10 17:15:08 +0000

    twoSum solution2

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
@@ -1,5 +1,7 @@
 package ca.jrvs.practice.codingChallenge;
 
+import java.util.HashMap;
+
 /**
  * Ticket: https://www.notion.so/jarvisdev/Two-Sum-067fee9f084e437caaf80d01c7078695
  */
@@ -18,6 +20,36 @@ public class TwoSum {
                 if(nums[firstIndex] + nums[secondIndex] == target){
                     return new int[] {firstIndex,secondIndex};
                 }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * hash search method
+     * Big-O: O(n)
+     * Justification: Go through array once
+     * @param nums
+     * @param target
+     * @return
+     * transform array into hashmap (key is value). For each (until match is found) item, find the possible match target-sum1=[sum2] ask if hashmap contains key of sum2. if found return value(index) as array
+     */
+    public int[] twoSumOne(int[] nums, int target){
+        HashMap<Integer,Integer> hashNums = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            hashNums.put(nums[i],i);
+        }
+        //foreach needs to finish, cannot return in the middle of operation
+//        hashNums.forEach((index, sum1) -> {
+//            int sum2 = target-sum1;
+//            if(hashNums.containsKey(sum2) && hashNums.get(sum2) != index){
+//                int[] ans = {index, hashNums.get(sum2)};
+//            }
+//        });
+        for (int index = 0; index < nums.length; index++) {
+            int sum2 = target-nums[index];
+            if(hashNums.containsKey(sum2) && hashNums.get(sum2) != index){
+                return new int[] {index, hashNums.get(sum2)};
             }
         }
         return null;
